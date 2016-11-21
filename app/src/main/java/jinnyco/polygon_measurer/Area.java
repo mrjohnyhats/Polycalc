37113f3f38c8c9e14a6bb2baff3c670639f12ae5
BuildTools <unconfigured@null.spigotmc.org>
2016-11-20 19:04:37 -0800

    finished FunctionActivity base class for various functions

--- app/src/main/java/jinnyco/polygon_measurer/Area.java
+++ app/src/main/java/jinnyco/polygon_measurer/Area.java
@@ -11,53 +11,49 @@ import android.widget.Toast;
  */
 
 public class Area extends FunctionActivity {
-    protected String[] input_heads = {"sides", "side lengths"};
-
-    protected TextWatcher watcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            int sides;
-            double len;
-            for(int i = 0; i < input_views.length; i++){
-                if(input_views[i].getText().toString().length() <= 0){
-                    Toast.makeText(Area.this, "please put something into each input box", Toast.LENGTH_SHORT);
-                    output.setText("0");
-                    return;
-                }
-                inputs[i] = input_views[i].getText().toString();
-            }
-            try {
-                sides = Integer.parseInt(inputs[0]);
-                len = Double.parseDouble(inputs[1]);
-            } catch(NumberFormatException e){
-                    return;
-            }
-            if (sides > 2 && len > 0) {
-                double t_len = len/2.0;
-                double angle = 2.0*Math.PI/sides;
-                double h = t_len / Math.tan(angle/2.0);
-                double area = (sides*len*h)/2.0;
-                Log.i("polygon finder", "sides = " + sides);
-                Log.i("polygon finder", "len = " + len);
-                output.setText(Double.toString(Math.round(area*10000.0)/10000.0));
-            } else {
-                output.setText("0");
-            }
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        input_heads = new String[]{"number of sides", "side length"};
+        watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int sides;
+                double len;
+                for(int i = 0; i < input_views.length; i++){
+                    if(input_views[i].getText().toString().length() <= 0){
+                        Toast.makeText(Area.this, "please put something into each input box", Toast.LENGTH_SHORT);
+                        output.setText("0");
+                        return;
+                    }
+                    inputs[i] = input_views[i].getText().toString();
+                }
+                try {
+                    sides = Integer.parseInt(inputs[0]);
+                    len = Double.parseDouble(inputs[1]);
+                } catch(NumberFormatException e){
+                    return;
+                }
+                if (sides > 2 && len > 0) {
+                    double t_len = len/2.0;
+                    double angle = 2.0*Math.PI/sides;
+                    double h = t_len / Math.tan(angle/2.0);
+                    double area = (sides*len*h)/2.0;
+                    output.setText(Double.toString(Math.round(area*10000.0)/10000.0));
+                } else {
+                    output.setText("0");
+                }
+            }
+        };
         super.onCreate(savedInstanceState);
     }
 }
--- app/src/main/java/jinnyco/polygon_measurer/FunctionActivity.java
+++ app/src/main/java/jinnyco/polygon_measurer/FunctionActivity.java
@@ -1,23 +1,43 @@
 package jinnyco.polygon_measurer;
 
 import android.app.Activity;
-import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextWatcher;
-import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.Gravity;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FunctionActivity extends Activity {
-    int input_quan;
     protected String[] input_heads, inputs;
-    EditText[] input_views;
+    protected EditText[] input_views;
     protected boolean has_pi_output = false;
-
     protected TextWatcher watcher;
     protected RelativeLayout dl;
+    protected TextView output;
+    protected TextView pi_output;
+
+    private TextView makeoutput(int i){
+        TextView o = new TextView(FunctionActivity.this);
+        RelativeLayout.LayoutParams oparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        if (input_views[input_views.length - 1] != null) {
+            oparams.addRule(RelativeLayout.BELOW, input_views[input_views.length - 1].getId());
+        }
+        oparams.setMargins(15, 15, 15, 15);
+
+        o.setId((int) Math.floor((Math.random()+0.1) * Math.pow(10.0, input_heads.length*2+i+1)));
+        o.setTextSize(50);
+        o.setTextColor(Color.BLACK);
+        o.setGravity(Gravity.CENTER);
+        o.setPadding(20, 20, 20, 20);
+        o.setLayoutParams(oparams);
+
+        return o;
+    }
 
 
     @Override
@@ -25,32 +45,66 @@ public class FunctionActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.default_funct);
 
-        input_views = new EditText[input_quan];
-        inputs = new String[input_quan];
-        input_heads = new String[input_quan];
+        input_views = new EditText[input_heads.length];
+        inputs = new String[input_heads.length];
 
         dl = (RelativeLayout) findViewById(R.id.defaultlayout);
 
+        //SET INPUTS
+
         TextView header;
         EditText input;
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams headerparams;
+        RelativeLayout.LayoutParams inputparams;
 
-        for (int i = 1; i < input_heads.length; i++) {
+        for (int i = 0; i < input_heads.length; i++) {
             header = new TextView(FunctionActivity.this);
+            header.setId((int)Math.floor((Math.random()+0.1)*Math.pow(10.0, i+1)));
+            header.setGravity(Gravity.CENTER);
+
             input = new EditText(FunctionActivity.this);
+            input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            input.setId((int)Math.floor((Math.random()+0.1)*Math.pow(10.0, i+1+input_heads.length)));
+
+            headerparams = new RelativeLayout.LayoutParams(params);
+            inputparams = new RelativeLayout.LayoutParams(params);
+
+            headerparams.setMargins(0, 50, 0, 0);
+            if(i > 0){
+                headerparams.addRule(RelativeLayout.BELOW, input_views[i-1].getId());
+            }
+            header.setLayoutParams(headerparams);
+            header.setTextSize(16);
             header.setText(input_heads[i]);
-            input.addTextChangedListener(watcher);
+            Log.e("mylog", "i = " + i);
+            Log.e("mylog", "input_heads[i] = " + input_heads[i]);
+
             dl.addView(header);
+
+            inputparams.addRule(RelativeLayout.BELOW, header.getId());
+            inputparams.setMargins(20, 10, 20, 10);
+            input.setLayoutParams(inputparams);
+
+            input.addTextChangedListener(watcher);
+
             dl.addView(input);
             input_views[i] = input;
-            inputs[i] = "";
         }
 
-        TextView output, pi_output;
+        //SET OUTPUTS
 
-        dl.addView(output);
-        if(has_pi_output){
-            pi_output.setText("0π");
-            dl.addView(pi_output);
+        if(input_views.length > 0) {
+            output = makeoutput(0);
+            output.setText("0");
+
+            dl.addView(output);
+
+            if (has_pi_output) {
+                pi_output = makeoutput(1);
+                pi_output.setText("0π");
+                dl.addView(pi_output);
+            }
         }
     }
 }
