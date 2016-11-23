d0fed7d2228d4b355e5b063cc68bf99bb3a1ac27
mrjohnyhats <jonbutterminecraftian@gmail.com>
2016-11-22 21:26:52 -0800

    added template class for making function creation easier (SHOULD NOT BE USED IN PROGRAM)

---
+++ app/src/main/java/jinnyco/polygon_measurer/Template.java
@@ -0,0 +1,15 @@
+package jinnyco.polygon_measurer;
+
+import android.os.Bundle;
+
+public class Template extends FunctionActivity {
+    @Override
+    protected void doFunction(){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
